a9ad9df2603ae93adbbb815cf8e1b584fdd8ca28
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-09-23 16:10:04 +0300

    initial commit

---
+++ DataTypes-lab/PoundsToDollars/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace PoundsToDollars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Create a program that converts British pounds to US dollars formatted to the 3rd decimal point. 1 British Pound = 1.31 Dollars*/
+            double pound = double.Parse(Console.ReadLine());
+            double dollars = (pound * 1.31);
+            Console.WriteLine($"{dollars:f3}");
+        }
+    }
+}
