54e994ea48c66a8cbf9f00c0db7f4b1a99d0a1d4
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-09-23 18:23:54 +0300

    commit

---
+++ DataTypes-lab/ConcatNames/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConcatNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Read two names and a delimiter. Print the names joined by the delimiter.*/
+            string name = Console.ReadLine();
+            string name2 = Console.ReadLine();
+            string delimiter = Console.ReadLine();
+            Console.Write($"{name}{delimiter}{name2}");
+
+        }
+    }
+}
