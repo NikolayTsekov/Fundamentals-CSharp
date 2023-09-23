2c13d1e151f6a9733863f78672d101d5dba12768
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-09-23 22:33:12 +0300

    commit

---
+++ DataTypesExercises/Elevator/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            int capacityOfPersons = int.Parse(Console.ReadLine());
+            int courses = (int)Math.Ceiling((double)numberOfPeople / capacityOfPersons);
+            Console.WriteLine(courses);
+
+        }
+    }
+}
