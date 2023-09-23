be7436d63a8a7522bbf5d810e2cd571af0b219b0
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-09-23 18:35:46 +0300

    commit

---
+++ DataTypes-lab/CharsToString/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CharsToString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Create a program that reads 3 lines of input. On each line, you get a single character. Combine all the characters into one string and print it on the console.*/
+            char first = char.Parse(Console.ReadLine());
+            char second = char.Parse(Console.ReadLine());
+            char third = char.Parse(Console.ReadLine());
+            Console.WriteLine($"{first}{second}{third}");
+            
+
+        }
+    }
+}
