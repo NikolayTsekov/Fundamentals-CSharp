352e0e332bb5401c8a45bc8e1192b680d40a8e30
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-09-23 22:23:15 +0300

    commit

---
+++ DataTypesExercises/IntegerOperations/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace IntegerOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int fourthNumber = int.Parse(Console.ReadLine());
+
+            int addingOperation = firstNumber + secondNumber;
+            int dividingOperation = addingOperation / thirdNumber;
+            int multiplyOperation = dividingOperation * fourthNumber;
+            Console.WriteLine(multiplyOperation);
+        }
+    }
+}
