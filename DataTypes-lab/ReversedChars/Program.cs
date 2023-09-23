3fb482b12ad2cc7d4ca351c840fa86630aae2306
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-09-23 18:17:51 +0300

    commit

---
+++ DataTypes-lab/ReversedChars/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ReversedChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Create a program that takes 3 lines of characters and prints them in reversed order with a space between them.*/
+            string input = Console.ReadLine();
+            string input2 = Console.ReadLine();
+            string input3 = Console.ReadLine();
+            Console.Write($"{input3} {input2} {input}");
+        }
+    }
+}
