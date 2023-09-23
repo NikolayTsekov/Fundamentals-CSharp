1522584c30377d2f984c859bd44aaa60f7f21e31
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-09-23 15:58:20 +0300

    commit

---
+++ DataTypes-lab/ConvertMetersToKilometers/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConvertMetersToKilometers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*You will be given an integer that will be a distance in meters. Create a program that converts meters to kilometers formatted to the second decimal point.*/
+            int meters = int.Parse(Console.ReadLine());
+            decimal kilometers = meters / 1000m;
+            Console.WriteLine($"{kilometers:f2}");
+
+        }
+    }
+}
