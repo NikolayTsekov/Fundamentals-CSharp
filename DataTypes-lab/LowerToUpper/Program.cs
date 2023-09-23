92bcb089a99b32664d01ffd4e3b993072d8bf762
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-09-23 18:40:47 +0300

    commit

---
+++ DataTypes-lab/LowerToUpper/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace LowerToUpper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Create a program that prints whether a given character is upper-case or lower case.*/
+            char letter = char.Parse(Console.ReadLine());
+            bool isUpper = char.IsUpper(letter);
+            if (isUpper == true)
+            {
+                Console.WriteLine("upper-case");
+            }
+            else
+            {
+                Console.WriteLine("lower-case");
+            }
+
+        }
+    }
+}
