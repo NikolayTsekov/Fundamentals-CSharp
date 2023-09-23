ecc6a65cf9c35b734f8337f8067d77a7e79ed5f3
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-09-23 17:39:48 +0300

    commit

---
+++ DataTypes-lab/ExactSumOfRealNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ExactSumOfRealNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Create a program to enter n numbers and calculate and print their exact sum (without rounding).*/
+            int n = int.Parse(Console.ReadLine());
+            decimal sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += decimal.Parse(Console.ReadLine()); 
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
