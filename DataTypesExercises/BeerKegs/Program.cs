69e1bf1c884c5c8e0dac9057ca91db74c5a2db7d
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-09-24 15:29:47 +0300

    commit

---
+++ DataTypesExercises/BeerKegs/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace BeerKegs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int kegs = int.Parse(Console.ReadLine());
+
+            string biggestKegsModel = string.Empty;
+            decimal biggestKegVolume = 0;
+
+            for (int i = 0; i < kegs; i++)
+            {
+                string kegsModel = Console.ReadLine();
+                decimal kegsRadius = decimal.Parse(Console.ReadLine());
+                decimal kegsHeight = decimal.Parse(Console.ReadLine());
+                decimal volume = (decimal)Math.PI * (kegsRadius * kegsRadius) * kegsHeight;
+                if (volume > biggestKegVolume)
+                {
+                    biggestKegsModel = kegsModel;
+                    biggestKegVolume = volume;
+                }
+            }
+            Console.WriteLine(biggestKegsModel);
+        }
+    }
+}
