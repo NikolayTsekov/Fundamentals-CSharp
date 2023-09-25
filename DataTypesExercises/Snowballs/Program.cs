f758965e08e46c10e33d248d5463aa83c51d00e9
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-09-25 22:01:16 +0300

    commit

---
+++ DataTypesExercises/Snowballs/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Numerics;
+
+namespace Snowballs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfSnowballs = int.Parse(Console.ReadLine());
+            BigInteger highestsnowballValue = 0;
+            int bestsnowballSnow = 0;
+            int bestsnowballTime = 0;
+            int bestsnowballQuality = 0;
+            
+            for (int i = 1; i <= numberOfSnowballs; i++)
+            {
+                int snowballSnow = int.Parse(Console.ReadLine());
+                int snowballTime = int.Parse(Console.ReadLine());
+                int snowballQuality = int.Parse(Console.ReadLine());
+                BigInteger snowballValue = BigInteger.Pow((snowballSnow / snowballTime), snowballQuality);
+                if (snowballValue > highestsnowballValue)
+                {
+                    highestsnowballValue = snowballValue;
+                    bestsnowballTime = snowballTime;
+                    bestsnowballQuality = snowballQuality;
+                    bestsnowballSnow = snowballSnow;
+                }
+            }
+
+            Console.WriteLine($"{bestsnowballSnow} : {bestsnowballTime} = {highestsnowballValue} ({bestsnowballQuality})");
+        }
+    }
+}
