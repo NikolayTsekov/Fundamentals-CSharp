892ad174171d7f8c148b2b447dfa03871a83a372
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-09-25 15:26:14 +0300

    commit

---
+++ DataTypesExercises/WaterOverflow/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace WaterOverflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int currentQuantity = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                int quantity = int.Parse(Console.ReadLine());
+                if (currentQuantity + quantity > 255)
+                {
+                    Console.WriteLine($"Insufficient capacity!");
+                }
+                else
+                {
+                    currentQuantity += quantity; 
+                }
+            }
+            Console.WriteLine($"{currentQuantity}");
+        }
+    }
+}
