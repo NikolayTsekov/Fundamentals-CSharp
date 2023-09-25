0d74aa9ec59d2eb961e0dfdaeb1c0906818a1e50
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-09-25 21:27:28 +0300

    commit

---
+++ DataTypesExercises/Pokemon/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Pokemon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int targets = 0;
+            int originalValue = n;
+            while (n >= m)
+            {
+                n -= m;
+                targets++;
+                if (n == originalValue * 0.50 && y != 0)
+                {
+                    n /= y;
+                }
+            }
+            Console.WriteLine($"{n}");
+            Console.WriteLine($"{targets}");
+        }
+    }
+}
