921a3f840c40f326bffb43a9b2b6f542f33260d1
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-09-25 16:09:38 +0300

    commit

---
+++ DataTypesExercises/SpiceMustFlow/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SpiceMustFlow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int yield = int.Parse(Console.ReadLine());
+            int days = 0;
+            int spices = 0;
+
+            while (yield >= 100)
+            {
+                days++;
+                spices += yield;
+                spices -= 26;
+                yield -= 10;
+            }
+            spices -= 26;
+            if (spices < 0) 
+            {
+                spices = 0;
+            }
+            Console.WriteLine($"{days}");
+            Console.WriteLine($"{spices}");
+        }
+    }
+}
