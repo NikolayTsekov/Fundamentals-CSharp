28b3b8b892d21c444d717ec8ef5845f4f0fca07b
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-09-30 14:23:52 +0300

    commit

---
+++ ArraysLab/DaysOfWeek/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DaysOfWeek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days = new string[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            int number = int.Parse(Console.ReadLine());
+
+            if (number < 1 || number > 7)
+            {
+                Console.WriteLine("Invalid day!");
+                return;
+            }
+            else
+            {
+                Console.WriteLine(days[number - 1]);
+            }
+        }
+    }
+}
