5a819832230f35b2c79f8ab484ec24561937f494
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-09-30 21:51:32 +0300

    commit

---
+++ ArraysLab/EqualsArrays/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace EqualsArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int[] numbers1 = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] != numbers1[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    return;
+                }
+                else
+                {
+                    sum += numbers[i];
+                }
+            }
+            Console.WriteLine($"Arrays are identical. Sum: {sum}");
+
+
+        }
+    }
+}
