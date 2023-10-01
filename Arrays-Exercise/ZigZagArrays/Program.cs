a6061161bf7295e18b941d79ed30522231271da1
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-10-01 14:31:16 +0300

    commit

---
+++ Arrays-Exercise/ZigZagArrays/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ZigZagArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] firstArray = new string[n];
+            string[] secondArray = new string[n];
+            bool isFirstArraySelected = true;
+            for (int i = 0; i < n; i++)
+            {
+                string numbers = Console.ReadLine();
+                string[] numbersArray = numbers.Split();
+
+                if (i % 2 == 0) 
+                {
+                    firstArray[i] = numbersArray[0];
+                    secondArray[i] = numbersArray[1];
+                }
+                else 
+                {
+                    firstArray[i] = numbersArray[1];
+                    secondArray[i] = numbersArray[0];
+                }
+                isFirstArraySelected = !isFirstArraySelected;
+            }
+            Console.WriteLine(string.Join(" ", firstArray));
+            Console.WriteLine(string.Join(" ", secondArray));
+
+        }
+    }
+}
