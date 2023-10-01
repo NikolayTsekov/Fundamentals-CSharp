aae93f071da6b124ded3ec0e7cf6ef52b33ada6d
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-10-01 15:17:13 +0300

    commit

---
+++ Arrays-Exercise/CommonElements/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CommonElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstArray = Console.ReadLine().Split();
+            string[] secondArray = Console.ReadLine().Split();
+
+            for (int i = 0; i < secondArray.Length; i++)
+            {
+                for (int j = 0; j < firstArray.Length; j++)
+                {
+                    if (secondArray[i] == firstArray[j])
+                    {
+                        Console.Write($"{firstArray[j]} ");
+                    }
+                }
+            }
+        }
+    }
+}
