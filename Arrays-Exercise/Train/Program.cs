9f1c569b60f5e19e041673b79bdb38b585e04369
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-10-01 13:27:28 +0300

    commit

---
+++ Arrays-Exercise/Train/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] passengers = new int[n];
+            for (int i = 0; i < passengers.Length; i++)
+            {
+                passengers[i] = int.Parse(Console.ReadLine());
+            }
+            int sum = 0;
+            for (int i = 0; i < passengers.Length; i++)
+            {
+                
+                Console.Write($"{passengers[i]} ");
+                sum += passengers[i];
+                
+            }
+            Console.WriteLine();
+            Console.WriteLine(sum);
+        }
+    }
+}
