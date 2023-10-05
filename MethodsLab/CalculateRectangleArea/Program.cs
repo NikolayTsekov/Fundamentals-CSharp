d9189e39296f966c0303ccefc944c84c1ebea667
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-10-05 22:25:29 +0300

    commit

---
+++ MethodsLab/CalculateRectangleArea/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CalculateRectangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double w = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+
+            double area = RectangleArea(w, h);
+            Console.WriteLine(area);
+        }
+        static double RectangleArea (double w, double h)
+        {
+            return w * h;
+        }
+    }
+}
