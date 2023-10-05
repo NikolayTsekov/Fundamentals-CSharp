65f1157fd0b90e6387ccf22430200515e2edb115
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-10-05 22:34:39 +0300

    commit

---
+++ MethodsLab/RepeatString/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace RepeatString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+
+            string result = RepeatString(text, n);
+            Console.WriteLine(result);
+        }
+        static string RepeatString(string text, int r)
+        {
+            string newText = " ";
+            for (int i = 0; i < r; i++)
+            {
+                newText += text;
+            }
+            return newText;
+        }
+    }
+}
