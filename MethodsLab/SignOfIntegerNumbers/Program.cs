bae177ebe966fe3664ba17acfc1a8810e99320e1
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-10-05 14:37:02 +0300

    commit

---
+++ MethodsLab/SignOfIntegerNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SignOfIntegerNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            NewMethod(number);
+        }
+
+        private static void NewMethod(int number)
+        {
+            if (number > 0)
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is zero.");
+            }
+        }
+    }
+}
