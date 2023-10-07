e590caab376bff4a8f9b9e17400ee0f5a74ecc94
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-10-07 18:23:25 +0300

    commit

---
+++ MethodsExercise/FactorialDivision/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace FactorialDivision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int seconfNum = int.Parse(Console.ReadLine());
+            double firstFactorial = Factorial(firstNum);
+            double secondFactorial = Factorial(seconfNum);
+            
+            Console.WriteLine($"{(firstFactorial / secondFactorial):f2}");
+        }
+
+        static double Factorial(int firstNum)
+        {
+            double firstFactorial = 1;
+            for (int i = 1; i <= firstNum; i++)
+            {
+                firstFactorial = firstFactorial * i;
+            }
+
+            return firstFactorial;
+        }
+    }
+}
