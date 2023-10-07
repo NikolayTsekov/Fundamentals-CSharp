97e51f951f7857e17f61ef6435e159a35156ca96
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-10-07 19:10:03 +0300

    commit

---
+++ MethodsExercise/PalindromeIntegers/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace PalindromeIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;  
+            while ((input = Console.ReadLine()) != "END")
+            {
+                bool isNumberPalindrome = isPalindrome(input);
+                Console.WriteLine(isNumberPalindrome);
+            }
+        }
+
+        static bool isPalindrome(string symbols)
+        {
+            int left = 0;
+            int right = symbols.Length - 1;
+            while (left < right)
+            {
+                if (symbols[left] != symbols[right])
+                {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+    }
+}
