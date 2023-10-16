13525e4f2d9d7d9ffb71b39adaad252da4b46b3a
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-10-16 16:06:37 +0300

    commit

---
+++ ListsExercise/ChangeList/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ChangeList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] arguments = input.Split();
+                if (arguments[0] == "Delete")
+                {
+                    int element = int.Parse(arguments[1]);
+                    list.Remove(element);
+                }
+                else if (arguments[0] == "Insert")
+                {
+                    int element = int.Parse(arguments[1]);
+                    int index = int.Parse(arguments[2]);
+                    list.Insert(index, element);
+                }
+            }
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
