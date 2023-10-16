e701a9f8d690b7e6a3395fb7973864632c9e54ef
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-10-16 21:54:12 +0300

    commit

---
+++ ListsExercise/HouseParty/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace HouseParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int gueatsCount = int.Parse(Console.ReadLine());
+            List<string> gueatList = new List<string>();
+
+            for (int i = 0; i < gueatsCount; i++)
+            {
+                string[] argument = Console.ReadLine().Split();
+                string name = argument[0]; 
+                if (argument[2] == "going!")
+                {
+                    if (gueatList.Contains(name)) // Exists(e=>e==name) // FindIndex(e=>e==name) != -1
+                    {
+                        Console.WriteLine($"{name} is already in the list!");
+                    }
+                    else
+                    {
+                        gueatList.Add(name);
+
+                    }
+
+                }
+                else if (argument[2] == "not")
+                {
+                    if (gueatList.Contains(name))
+                    {
+                        gueatList.Remove(name);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{name} is not in the list!");
+                    }
+                }
+            }
+            foreach (string guest in gueatList)
+            {
+                Console.WriteLine(guest);
+            }
+            
+        }
+    }
+}
