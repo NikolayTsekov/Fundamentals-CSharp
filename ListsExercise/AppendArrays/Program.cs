cf7bb6f927b441de690e5326aa21b96659c33245
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-10-17 09:33:14 +0300

    commit

---
+++ ListsExercise/AppendArrays/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace AppendArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] stringArrays = Console.ReadLine()
+                .Split('|', StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> symbols = ExtractSymbols(stringArrays);
+            Console.WriteLine(string.Join(" ", symbols));
+        }
+
+        private static List<string> ExtractSymbols(string[] stringArrays)
+        {
+            List<string> result = new List<string>();
+            for (int i = stringArrays.Length-1; i >= 0; i--)
+            {
+                string str = stringArrays[i];
+                string[] array = str.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                result.AddRange(array);
+
+            }
+            
+            return result;
+        }
+    }
+}
