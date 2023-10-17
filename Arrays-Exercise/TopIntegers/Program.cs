fd8948e01ab3b63921c4b0f47bbb25ed29858d99
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-10-17 15:09:03 +0300

    commit

---
+++ Arrays-Exercise/TopIntegers/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace TopIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i+1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] <= numbers[j])
+                    {
+                        break;
+                    }
+                    else if (j == numbers.Length - 1)
+                    {
+                        Console.Write($"{numbers[i]} ");
+                    }
+                }
+            }
+
+            Console.WriteLine(numbers[numbers.Length-1]);
+        }
+    }
+}
