4219d4c39a3c42565e95261d5a424cee71f6f061
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-10-18 17:12:11 +0300

    commit

---
+++ Arrays-Exercise/ArrayStatistics/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayStatistics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int minNumber = int.MaxValue;
+            int maxNumber = int.MinValue;
+            int sum = 0;
+            double average = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                int currentNumber = array[i];
+                if (currentNumber > maxNumber)
+                {
+                   maxNumber = currentNumber;
+                }
+                if (currentNumber < minNumber)
+                {
+                    minNumber = currentNumber;
+                }
+                sum += currentNumber;
+                average = ((double) sum / array.Length);
+            }
+
+            Console.WriteLine($"Min = {minNumber}");
+            Console.WriteLine($"Max = {maxNumber}");
+            Console.WriteLine($"Sum = {sum}");
+            Console.WriteLine($"Average = {average}");
+
+        }
+    }
+}
