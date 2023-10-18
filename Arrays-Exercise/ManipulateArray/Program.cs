578b4e61c9aed14befe4758c22d7e86ce716f400
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-10-18 19:31:51 +0300

    commit

---
+++ Arrays-Exercise/ManipulateArray/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Net.NetworkInformation;
+
+namespace ManipulateArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] stringArray = Console.ReadLine()
+                .Split(' ')
+                .ToArray();
+            var changeArray = stringArray;
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string command = Console.ReadLine();
+                if (command == "Distinct")
+                {
+                    changeArray = changeArray.Distinct().ToArray();
+
+                }
+
+                if (command == "Reverse")
+                {
+                    Array.Reverse(changeArray);
+                }
+
+                if (command != "Distinct" && command != "Reverse")
+                {
+                    string[] replace = command.Split(' ').ToArray();
+                    changeArray[int.Parse(replace[1])] = replace[2];
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", changeArray));
+
+        }
+    }
+}
