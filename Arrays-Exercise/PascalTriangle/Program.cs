7ca1d88e74dadc90e0671229dff107a31db6fb49
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-10-18 12:03:00 +0300

    commit

---
+++ Arrays-Exercise/PascalTriangle/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace PascalTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            var pascalTriangle = new int[rows][];
+
+            pascalTriangle[0] = new int[1];
+            pascalTriangle[0][0] = 1;
+            for (int row = 1; row < rows; row++)
+            {
+                pascalTriangle[row] = new int[row + 1];
+                pascalTriangle[row][0] = 1;
+                pascalTriangle[row][pascalTriangle[row].Length - 1] = 1;
+                for (int col = 1; col < pascalTriangle[row].Length-1; col++)
+                {
+                    pascalTriangle[row][col] = pascalTriangle[row - 1][col - 1] + pascalTriangle[row - 1][col];
+                }
+            }
+
+            foreach (var row in pascalTriangle)
+            {
+                Console.WriteLine(string.Join(" ", row));
+            }
+        }
+    }
+}
