a842b26a1db24e6da148a043b35b58ce259a0c09
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-10-19 14:06:27 +0300

    commit

---
+++ MidExamPreparation/TheLift/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace TheLift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleWaiting = int.Parse(Console.ReadLine());
+            int[] array = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int currentWagon = array[i];
+                if (currentWagon < 4)
+                {
+                    int emptySeats = 4 - array[i];
+                    peopleWaiting -= emptySeats;
+                    if (peopleWaiting<0)
+                    {
+                        array[i] = 4 - Math.Abs(peopleWaiting);
+                        Console.WriteLine("The lift has empty spots!");
+                        Console.WriteLine(string.Join(" ", array));
+                        return;
+                    }
+
+                    array[i] = 4;
+                }
+            }
+
+            if (peopleWaiting!=0)
+            {
+                Console.WriteLine($"There isn't enough space! {peopleWaiting} people in a queue!");
+            }
+
+            
+            Console.WriteLine(string.Join(" ", array));
+        }
+    }
+}
