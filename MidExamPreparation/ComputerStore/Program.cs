84bfa7d1c048c9e0eebf3d1ebb984e17bde956f3
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-10-19 22:07:57 +0300

    commit

---
+++ MidExamPreparation/ComputerStore/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ComputerStore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            decimal totalPrice = 0;
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input == "special" || input == "regular")
+                {
+                    break;
+                }
+
+                decimal price = decimal.Parse(input); 
+                if (price < 0)
+                {
+                    Console.WriteLine("Invalid price!");
+                    continue;
+                }
+                totalPrice += price;
+               
+            }
+
+            if (totalPrice == 0)
+            {
+                Console.WriteLine("Invalid order!");
+            }
+            else
+            {
+                decimal taxes = totalPrice * (20m/100m); 
+                Console.WriteLine("Congratulations you've just bought a new computer!");
+                Console.WriteLine($"Price without taxes: {totalPrice:f2}$");
+                Console.WriteLine($"Taxes: {taxes:f2}$");
+                Console.WriteLine("-----------");
+                decimal priceWithTaxes = totalPrice + taxes;
+                if (input == "special")
+                {
+                    priceWithTaxes -= (priceWithTaxes * 10m)/100m;
+                }
+                Console.WriteLine($"Total price: {priceWithTaxes:f2}$");
+            } 
+           
+        }
+    }
+}
