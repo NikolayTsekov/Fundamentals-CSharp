0c4bb861677913612ec03b5568d42abe5fc4463b
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-10-27 22:19:58 +0300

    commit

---
+++ ObjectsAndClassesLab/BigFactorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Numerics;
+
+namespace BigFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            BigInteger factorial = 1;
+            for (int i = n; i >= 2; i--)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine(factorial);
+        }
+    }
+}
