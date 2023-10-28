3b9e5b03f0c82db4bba0cd0edf57d9cb1a5ce6a4
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-10-28 22:18:34 +0300

    commit

---
+++ ObjectsAndClassesLab/Students2.0/Program.cs
@@ -0,0 +1,75 @@
+﻿using static Students2._0.Program;
+
+namespace Students2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = default;
+            List<Student> students = new List<Student>();
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] tokens = input.Split();
+                string firstName = tokens[0];
+                string lastName = tokens[1];
+                int age = int.Parse(tokens[2]);
+                string hometown = tokens[3];
+
+                Student student = new Student();
+                student.FirstName = firstName;
+                student.LastName = lastName;
+                student.Age = age;
+                student.HomeTown = hometown;
+
+                int index = ValidateStudent(firstName, lastName, students);
+                if (index != -1)
+                {
+                    students[index].Age = age;
+                    students[index].HomeTown = hometown;
+                    continue;
+                }
+                else
+                {
+                    students.Add(student);
+
+                }
+
+
+
+            }
+            string cityFilter = Console.ReadLine();
+            foreach (Student student in students)
+            {
+                if (student.HomeTown == cityFilter)
+                {
+                    Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+                }
+                
+            }
+        }
+
+        static int ValidateStudent(string firstName, string lastName, List<Student> students)
+        {
+            for (int i = 0; i < students.Count; i++)
+            {
+                if (students[i].FirstName == firstName && students[i].LastName == lastName)
+                {
+                    return i;
+                }
+            }
+            
+
+            return -1;
+        }
+
+        public class Student
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int Age { get; set; }
+            public string HomeTown { get; set; }
+        }
+    }
+}
