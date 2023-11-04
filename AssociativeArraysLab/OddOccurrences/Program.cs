6a2db4402ed63677a9b1e6e912c5c84dd0a5e679
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-11-04 12:03:18 +0200

    commit

---
+++ AssociativeArraysLab/OddOccurrences/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace OddOccurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split(" ")
+                .Select(x => x.ToLower())
+                .ToArray();
+            Dictionary<string, int> numberOccurences = new Dictionary<string, int>();
+            
+            for (int i = 0; i < words.Length; i++)
+            {
+                string currentWord = words[i];
+                if (!numberOccurences.ContainsKey(currentWord))
+                {
+                    numberOccurences.Add(currentWord, 0);
+                }
+
+                numberOccurences[currentWord]++;
+            }
+
+            foreach (KeyValuePair<string, int> word in numberOccurences)
+            {
+                if (word.Value%2==1)
+                {
+                    Console.Write($"{word.Key} ");
+
+                }
+            }
+
+        }
+    }
+}
