13a3ce81f71aa401d2da2b02b5a7353ffe5aa960
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-11-04 13:36:23 +0200

    commit

---
+++ AssociativeArraysLab/WordSynonyms/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace WordSynonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var dictionary = new Dictionary<string, string>();
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+                
+                if (!dictionary.ContainsKey(word))
+                {
+                    dictionary[word] = synonym;
+                }
+                else
+                {
+                    dictionary[word] += ", " + synonym;
+                }
+            }
+
+            foreach (KeyValuePair<string, string> item in dictionary)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value}");
+            }
+        }
+    }
+}
