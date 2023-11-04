e60ea0d98ba0e1f54b6dd3e7e77643e2990dd880
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-11-04 23:09:56 +0200

    commit

---
+++ AssociativeArraysExercise/CountChars InAString/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace CountChars_InAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            
+            Dictionary<char, int> charOccurrences = new Dictionary<char, int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                char character = input[i];
+                if (character == ' ')
+                {
+                    continue;
+                }
+                if (!charOccurrences.ContainsKey(character))
+                {
+                    charOccurrences.Add(character, 0);
+                }
+
+                charOccurrences[character]++;
+            }
+
+            foreach (var charOccurrence in charOccurrences)
+            {
+                char character = charOccurrence.Key;
+                int count = charOccurrence.Value;
+                Console.WriteLine($"{character} -> {count}");
+            }
+        }
+    }
+}
