4db6a62c69dd313b3f516d76188478f90b126493
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-11-05 20:53:32 +0200

    commit

--- AssociativeArraysExercise/AMinerTask/Program.cs
+++ AssociativeArraysExercise/AMinerTask/Program.cs
@@ -4,18 +4,14 @@
     {
         static void Main(string[] args)
         {
-            //2. Създавам речник
             Dictionary<string, uint> resourceMap = new Dictionary<string, uint>();
-            //1. Създавам while цикъл
             string input;
             while ((input = Console.ReadLine()) != "stop")
             {
-                //3.Проверявам дали има такъв ключ вече
                 if (!resourceMap.ContainsKey(input))
                 {
                     resourceMap.Add(input, 0);
                 }
-                //4.Чета количеството
                 var quantity = uint.Parse(Console.ReadLine());
 
                 resourceMap[input] += quantity;
