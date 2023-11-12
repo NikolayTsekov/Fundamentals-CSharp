4c53832b2bab9ca3fde553e203e9173055aea492
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-11-12 22:29:48 +0200

    commit

---
+++ TextProcessingLab/RepeatStrings/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace RepeatStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split().ToArray();
+            string result = string.Empty;
+            foreach (string word in words)
+            {
+                int currentWordLength = word.Length;
+                for (int i = 0; i < currentWordLength; i++)
+                {
+                    result += word;
+                }
+            }
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
