b62301d1ef18e90edf8f4ed2832e3755e8a08b06
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-11-12 22:20:09 +0200

    commit

---
+++ TextProcessingLab/ReverseStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ReverseStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = String.Empty;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string reversed = "";
+                for (int i = command.Length - 1; i >= 0; i--)
+                {
+                    reversed += command[i];
+                }
+                Console.WriteLine($"{command} = {reversed}");
+
+            }
+
+        }
+    }
+}
