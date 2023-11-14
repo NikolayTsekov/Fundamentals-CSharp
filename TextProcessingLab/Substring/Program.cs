41c9084e46796ca767d2d08761854ad5972d6db7
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-11-14 21:08:11 +0200

    commit

---
+++ TextProcessingLab/Substring/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filter = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            while (text.Contains(filter))
+            {
+                text = text.Remove(text.IndexOf(filter), filter.Length);
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
