5aedf1b342e0fc302760b9921def1e08dbe27b2f
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-11-15 21:36:47 +0200

    commit

---
+++ TextProcessingExercise/ExtractFile/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ExtractFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = Console.ReadLine();
+
+            string fileName = String.Empty;
+            string fileExtension = String.Empty;
+
+            int lastSeparator = file.LastIndexOf('\\');
+            int extensionIndex = file.LastIndexOf(".");
+            if (lastSeparator != -1 && extensionIndex != -1 && lastSeparator < extensionIndex)
+            {
+                fileName = file.Substring(lastSeparator+1, extensionIndex-lastSeparator-1);
+                fileExtension = file.Substring(extensionIndex + 1);
+            }
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {fileExtension}");
+        }
+    }
+}
