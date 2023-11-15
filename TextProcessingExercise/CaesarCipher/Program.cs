068583b7e50a49f9289154f1bcfc3f53fc2cc572
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-11-15 21:51:43 +0200

    commit

---
+++ TextProcessingExercise/CaesarCipher/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace CaesarCipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            StringBuilder cipher = new StringBuilder();
+            for (int i = 0; i < text.Length; i++)
+            {
+                char original = text[i];
+                cipher.Append((char)(original+3));
+            }
+
+            Console.WriteLine(cipher);
+        }
+    }
+}
