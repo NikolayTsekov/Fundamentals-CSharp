41e66205f05cceddd41ba3ce0ecd0233b1788c25
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-11-16 13:25:42 +0200

    commit

---
+++ TextProcessingExercise/StringExplosion/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+
+namespace StringExplosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string result = ProcessExplosions(input);
+            Console.WriteLine(result);
+
+        }
+
+        private static string ProcessExplosions(string input)
+        {
+            StringBuilder resultBuilder = new StringBuilder();
+            int strenght = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i]=='>')
+                {
+                    strenght += int.Parse(input[i + 1].ToString());
+                    resultBuilder.Append(input[i]);
+
+                }
+                else if (strenght==0)
+                {
+                    resultBuilder.Append(input[i]);
+                }
+                else
+                {
+                    strenght--;
+                }
+            }
+            return resultBuilder.ToString();
+        }
+    }
+}
