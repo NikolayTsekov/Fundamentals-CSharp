58fc3d5f970b3cbb61d17925b312de4eba073530
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-11-26 21:58:06 +0200

    commit

---
+++ ExamPreparation/SecretChat/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace SecretChat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretMessage = Console.ReadLine();
+
+            string line;
+            while ((line = Console.ReadLine()) != "Reveal")
+            {
+                string[] tokens = line.Split(":|:");
+                string command = tokens[0];
+                if (command == "InsertSpace")
+                {
+                    int index = int.Parse(tokens[1]);
+                    secretMessage = secretMessage.Insert(index, " ");
+                }
+                else if (command == "ChangeAll")
+                {
+                    string substring = tokens[1];
+                    string replacement = tokens[2];
+                    secretMessage = secretMessage.Replace(substring, replacement);
+                }
+                else 
+                {
+                    string substring = tokens[1];
+                    int substringIndex = secretMessage.IndexOf(substring);
+                    if (substringIndex == -1)
+                    {
+                        Console.WriteLine("error");
+                        continue;
+                    }
+                    secretMessage = secretMessage.Remove(substringIndex, substring.Length);
+                    string reverseSubstring = new string(substring.Reverse().ToArray());
+                    secretMessage += reverseSubstring;
+                }
+                Console.WriteLine(secretMessage);
+
+            }
+            Console.WriteLine($"You have a new text message: {secretMessage}");
+        }
+    }
+}
