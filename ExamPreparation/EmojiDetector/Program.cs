6869d4d650f2eb1e77816f9307eb8c7cf7c222f8
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-11-28 14:55:16 +0200

    commit

---
+++ ExamPreparation/EmojiDetector/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+
+namespace EmojiDetector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string coolThresholdPatter = @"\d";
+            string emojiPattern = @"(\*{2}|\:{2})(?<Emoji>[A-Z][a-z]{2,})\1";
+            List<string> coolEmojies = new List<string>();
+
+            string input = Console.ReadLine();
+            ulong coolThreshold = 1;
+
+            foreach (Match match in Regex.Matches(input, coolThresholdPatter))
+            {
+                coolThreshold *= ulong.Parse(match.Value);
+            }
+            MatchCollection matches = Regex.Matches(input, emojiPattern);
+            foreach (Match match in matches)
+            {
+                string emojiStr = match.Groups["Emoji"].Value;
+
+                ulong totalEmojies = 0;
+                foreach (char character in emojiStr)
+                {
+                    totalEmojies += character;
+                }
+                if (totalEmojies > coolThreshold)
+                {
+                    coolEmojies.Add(match.Value);
+                }
+            }
+            Console.WriteLine($"Cool threshold: {coolThreshold}");
+            Console.WriteLine($"{matches.Count} emojis found in the text. The cool ones are:");
+            coolEmojies.ForEach(emoji => Console.WriteLine(emoji));
+        }
+    }
+}
