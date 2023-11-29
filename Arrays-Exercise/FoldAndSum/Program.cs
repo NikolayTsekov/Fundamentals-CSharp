0b28e44f26371037d27282de13a670fc33273205
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-11-29 13:14:17 +0200

    commit

---
+++ Arrays-Exercise/FoldAndSum/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace FoldAndSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            int leftIndex = array.Length / 4 - 1;
+            int rightIndex = 3 * array.Length / 4;
+
+            int[] topArray = new int[array.Length/2];
+            int numbersSoFar = 0;
+            for (int i = leftIndex; i >=0; i--) 
+            {
+                numbersSoFar++;
+                topArray[leftIndex-i] = array[i];
+            }
+            for (int i = array.Length-1; i >=rightIndex; i--)
+            {
+                topArray[array.Length - 1 - i + numbersSoFar] = array[i];
+            }
+            int[] bottomArray = new int[array.Length/2];
+            for (int i = leftIndex+1;i <rightIndex;i++)
+            {
+                bottomArray[i-numbersSoFar] = array[i];
+            }
+            for (int i = 0; i < topArray.Length; i++)
+            {
+                Console.Write(topArray[i] + bottomArray[i] + " ");
+            }
+        }
+    }
+}
