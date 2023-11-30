cde77bd209873a513c21be32d3dbb2a4248564a8
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-11-30 10:34:53 +0200

    commit

---
+++ ExamPreparation/AdAstra/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdAstra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"([#\|])(?<name>[A-Za-z\s]+)\1(?<date>\d{2}\/\d{2}\/\d{2})*\1(?<calories>\d{1,5})\1";
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+            int totalCalories = 0;
+
+            foreach (Match match in matches) 
+            {
+                int calories = (int.Parse)(match.Groups["calories"].Value);
+                totalCalories += calories;
+            }
+            int days = totalCalories / 2000;
+            Console.WriteLine($"You have food to last you for: {days} days!");
+            foreach (Match match in matches)
+            {
+                Console.WriteLine($"Item: {match.Groups["name"].Value}, Best before: {match.Groups["date"].Value}, Nutrition: {match.Groups["calories"].Value}");
+            }
+        }
+    }
+}
