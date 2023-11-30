be1b513692d245bd255c9105dd9cfe94fff5dbb0
NikolayTsekov <130857301+NikolayTsekov@users.noreply.github.com>
2023-11-30 15:47:18 +0200

    commit

---
+++ ExamPreparation/DestinationMapper/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace DestinationMapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"([=/])([A-Z][A-Za-z]{2,})\1";
+            Regex regex = new Regex(pattern, RegexOptions.Compiled);
+            List<string> destinations = new List<string>();
+            int travelPoints = 0;
+            foreach (Match match in regex.Matches(input)) 
+            {
+                travelPoints += match.Groups[2].Length;
+                string currentDestination = match.Groups[2].Value;
+                destinations.Add(currentDestination);
+            }
+            string destinationList = String.Join(", ", destinations);
+            Console.WriteLine($"Destinations: " + destinationList);
+            Console.WriteLine($"Travel Points: " + travelPoints);
+        }
+    }
+}
